958df43e346084faddbd708da415104df6e04694
Sergio Vanovi <sergiovanovi@mail.ru>
2022-01-03 13:21:49 +0300

    add pool

--- src/main/java/com/sergiovanovi/react/config/DbConfig.java
+++ src/main/java/com/sergiovanovi/react/config/DbConfig.java
@@ -1,14 +1,18 @@
 package com.sergiovanovi.react.config;
 
-import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
-import io.r2dbc.postgresql.PostgresqlConnectionFactory;
+import io.r2dbc.spi.ConnectionFactories;
 import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.ConnectionFactoryOptions;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.r2dbc.core.DatabaseClient;
+
+import static io.r2dbc.pool.PoolingConnectionFactoryProvider.INITIAL_SIZE;
+import static io.r2dbc.pool.PoolingConnectionFactoryProvider.MAX_SIZE;
+import static io.r2dbc.spi.ConnectionFactoryOptions.*;
 
 @Data
 @Configuration
@@ -21,16 +25,22 @@ public class DbConfig {
     private String dataBase;
     private String username;
     private String password;
+    private int initPoolSize;
+    private int maxPoolSize;
 
 
     @Bean
-    public PostgresqlConnectionFactory connectionFactory() {
-        return new PostgresqlConnectionFactory(PostgresqlConnectionConfiguration.builder()
-                .host(host)
-                .port(port)
-                .database(dataBase)
-                .username(username)
-                .password(password)
+    public ConnectionFactory connectionFactory() {
+        return ConnectionFactories.get(ConnectionFactoryOptions.builder()
+                .option(DRIVER,"pool")
+                .option(PROTOCOL,"postgresql") // driver identifier, PROTOCOL is delegated as DRIVER by the pool.
+                .option(HOST,host)
+                .option(PORT,port)
+                .option(DATABASE,dataBase)
+                .option(USER,username)
+                .option(PASSWORD,password)
+                .option(INITIAL_SIZE, initPoolSize)
+                .option(MAX_SIZE, maxPoolSize)
                 .build());
     }
 
